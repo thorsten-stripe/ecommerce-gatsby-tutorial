f979c146186e0bc098a03160328c2396acca2102
Thor <thorsten-stripe@users.noreply.github.com>
2019-01-03 14:28:11 +0100

    Remove cart from advanced example (#1)
    * Remove Cart from advanced example
    
    * Simplify success page

--- src/components/Products/Skus.js
+++ src/components/Products/Skus.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import SkuCard from './SkuCard'
 
@@ -10,33 +10,48 @@ const conatinerStyles = {
   padding: '1rem 0 1rem 0',
 }
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query SkusForProduct {
-        skus: allStripeSku(
-          filter: { product: { id: { eq: "prod_EGl7ZnT96XrPf6" } } }
-          sort: { fields: [price] }
-        ) {
-          edges {
-            node {
-              id
-              currency
-              price
-              attributes {
-                name
+class Skus extends Component {
+  // Initialise Stripe.js with your publishable key.
+  // You can find your key in the Dashboard:
+  // https://dashboard.stripe.com/account/apikeys
+  state = {
+    stripe: window.Stripe('pk_test_qOUT7QyfVVOJlWXtnbzzZ9Tn', {
+      betas: ['checkout_beta_4'],
+    }),
+  }
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SkusForProduct {
+            skus: allStripeSku(
+              filter: { product: { id: { eq: "prod_EGl7ZnT96XrPf6" } } }
+              sort: { fields: [price] }
+            ) {
+              edges {
+                node {
+                  id
+                  currency
+                  price
+                  attributes {
+                    name
+                  }
+                }
               }
             }
           }
-        }
-      }
-    `}
-    render={({ skus }) => (
-      <div style={conatinerStyles}>
-        {skus.edges.map(({ node: sku }) => (
-          <SkuCard {...props} key={sku.id} sku={sku} />
-        ))}
-      </div>
-    )}
-  />
-)
+        `}
+        render={({ skus }) => (
+          <div style={conatinerStyles}>
+            {skus.edges.map(({ node: sku }) => (
+              <SkuCard key={sku.id} sku={sku} stripe={this.state.stripe} />
+            ))}
+          </div>
+        )}
+      />
+    )
+  }
+}
+
+export default Skus
--- src/pages/advanced.js
+++ src/pages/advanced.js
@@ -4,17 +4,13 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-import Cart from '../components/Cart'
 import Skus from '../components/Products/Skus'
 
 const AdvancedExamplePage = () => (
   <Layout>
     <SEO title="Advanced Example" />
     <h1>This is the advanced example</h1>
-    <h2>With multiple variants and easy cart functionality</h2>
-    <Cart>
-      <Skus />
-    </Cart>
+    <Skus />
     <Link to="/">Go back to the easy example</Link>
   </Layout>
 )
