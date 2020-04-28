4dc0e78d554db2d1c764b548af65370babe1cc19
Thorsten Schaeff <thorsten@stripe.com>
2020-04-28 15:45:42 +0800

    Update cart example to use-shopping-cart

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,8 @@ import Header from './header'
 import './layout.css'
 import stripeLogo from '../images/powered_by_stripe.svg'
 
+import '@stripe/stripe-js' // https://github.com/stripe/stripe-js#import-as-a-side-effect
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
