0346c66da32b987ce0277593259f752cfdec9be1
Thorsten Schaeff <thorsten@stripe.com>
2020-04-29 18:35:50 +0800

    Update success page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-class SecondPage extends Component {
-  componentDidMount() {
-    // Empty localStorage after successful payment.
-    localStorage.removeItem('stripe_checkout_items')
-  }
-
-  render() {
-    return (
-      <Layout>
-        <SEO title="Payment Success" />
-        <h1>Sucess!</h1>
-        <Link to="/">Shop again</Link>
-      </Layout>
-    )
-  }
+const SuccessPage = () => {
+  return (
+    <Layout>
+      <SEO title="Payment Success" />
+      <h1>Sucess!</h1>
+      <Link to="/">Shop again</Link>
+    </Layout>
+  )
 }
 
-export default SecondPage
+export default SuccessPage
